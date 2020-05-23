c752e309c89e372e58d5e445f232e77fc365c423
ken <c.uvsuv@163.com>
2020-05-23 23:32:45 +0800

    push version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-hub checkout master
+hub checkout ${BRANCH}
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./app/build.gradle`
 hub release create -a ./app/release/outputs/apk/*-release.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S) 
