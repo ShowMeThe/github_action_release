427bca17d0a732efa5be9d93727b9e60b919fcc4
ken <c.uvsuv@163.com>
2020-05-24 20:31:44 +0800

    push version

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 hub checkout ${BRANCH}
 #hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release.apk -m "Release - ${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
-local=${'./app/build/outputs/apk/release/*-release.apk'}
+var='./app/build/outputs/apk/release/*-release.apk'
+local="$(find  -wholename $var)"
+echo $local
 curl -F "file=@$local" \
         -F "uKey=${uKey}" \
         -F "_api_key=${apiKey}" \
