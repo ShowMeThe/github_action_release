45c5a4e9ed5f3473fb2769395dd1b8aadcf28c1f
ken <c.uvsuv@163.com>
2020-05-24 12:47:23 +0800

    push version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ hub checkout ${BRANCH}
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release.apk -m "Release - ${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
 local='./app/build/outputs/apk/release/*-release.apk'
 echo $local
-curl -F "file=@${${local}}" \
+curl -F "file=@${local}" \
         -F "uKey=${uKey}" \
         -F "_api_key=${apiKey}" \
         https://www.pgyer.com/apiv1/app/upload
